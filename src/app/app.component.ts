b49dc9e071a220bc179a9a5d04dbdc0a6ad7f939
erman <erman@bumin.io>
2018-08-10 17:59:39 +0300

    aslanım benim çalıştı

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="container">
-      <a
-        routerLink="/login">Login</a> |
-      <a
-        routerLink="/merchant">Merchant</a> |
-      <a
-        routerLink="/user">User</a>
-      <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: 'layout.html'
 })
 export class AppComponent {
 
